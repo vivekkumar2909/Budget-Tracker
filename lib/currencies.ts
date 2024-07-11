3eba33a264f2b3292d2affe4570d84e2e048c505
vivekkumar2909 <rajvivek620065@gmail.com>
2024-07-11 21:27:25 +0530

    Final of this project

---
+++ lib/currencies.ts
@@ -0,0 +1,9 @@
+export const Currencies = [
+  { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "EUR", label: "€ Euro", locale: "de-DE" },
+  { value: "IND", label: "€ Rupee", locale: "INR" },
+  { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
+  { value: "GBP", label: "£ Pound", locale: "en-GB" },
+];
+
+export type Currency = (typeof Currencies)[0];
